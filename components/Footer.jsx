03c5706625a5790d8fe8b9d0eb6047505c6565f3
Fredrickmureti <121539435+Fredrickmureti@users.noreply.github.com>
2025-01-13 04:00:21 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className='flex-between w-full mt-16 pt-3 border-t border-gray-200'>
             <p className='text-center text-gray-600'>
                 Created with <span className='text-red-500'>❤️</span> by
-                <Link href='https://devfrderick.me' className='text-green-500 ml-1'>
+                <Link href='https://www.devfredrick.me/' className='text-green-500 ml-1'>
                     devfrderick
                 </Link>
             </p>
